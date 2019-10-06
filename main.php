d4cbb4cfc581dfd54f42e25dc58b85dabe6cdce9
Vincent <a@b.c>
2019-10-06 18:16:49 +0200

    Add more complex example to illustrate model

--- main.php
+++ main.php
@@ -15,4 +15,52 @@ foreach(Factory::event(10) as $e)
 // Construct the final state.
 $state = State::construct($queue);
 
-print($state);
+print("random run: " . $state . "\n");
+
+class ComplexPrediction implements IEvent {
+    public $event;
+    public $callable;
+    public function __construct(IEvent $e, Callable $f) {
+        $this->event = $e;
+        $this->callable = $f;
+    }
+
+    public function resolve(State $s) : State {
+        $newS = $this->event->resolve($s);
+        if(($this->callable)($newS)) {
+            $newS->state *= 2;
+            return $newS;
+        }
+
+        return $s;
+    }
+};
+
+// Some more complex behaviour
+$terminator = new class implements IEvent {
+    public function resolve(State $s) : State {
+        return $s;
+    }
+};
+
+// Make a decision after inspecting the current state
+$prediction1Func = function ($s) {
+    return $s->state == 6;
+};
+
+// Make a decision after inspecting the current state
+$prediction2Func = function ($s) {
+    if ($s->state > 10 || $s->lastUpdated == 1) {
+        return true;
+    }
+    return false;
+};
+
+$record = new Record($terminator); // S->state = 6
+$prediction1 = new ComplexPrediction($record, $prediction1Func); // S->state = 12
+$prediction2 = new ComplexPrediction($prediction1, $prediction2Func); // S->state = 24
+
+$q2 = new Queue();
+$q2->add($prediction2);
+$q2->add($record); // S->state = 25
+print("Complex prediction: " . State::construct($q2) . "\n");
